1d16789528d50b7fa8d161364bbaf7455b87a278
punkestu <pangestubima89@gmail.com>
2021-01-31 18:56:04 +0700

    membuat sistem collision dan manata map + minor

--- main.cpp
+++ main.cpp
@@ -8,102 +8,116 @@ struct vect2d{
 };
 
 std::vector<bool> _maps = {
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+    0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,
+    0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,
+    0,1,0,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,0,1,0,
+    0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,
+    0,1,0,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,0,1,0,
+    0,1,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,1,0,
+    0,1,1,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,1,0,
+    0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,
+    1,1,1,1,1,0,1,0,1,1,0,1,1,0,1,0,1,1,1,1,1,
+    0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,
+    1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,
+    0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,
+    0,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,0,
+    0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,
+    0,1,0,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,0,1,0,
+    0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,
+    0,1,1,0,1,0,1,0,1,1,1,1,1,0,1,0,1,0,1,1,0,
+    0,1,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,1,0,
+    0,1,0,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,0,1,0,
+    0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,
+    0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0
 };
 
 class player{
 private:
-    SDL_Rect body;
-    SDL_Rect eye;
+    SDL_Rect body, mouth;
     bool moveD[4];
 public:
     player(std::vector<bool>* maps){
-        body = {100,100,30,30};
-        eye = {body.x+20, body.y+5, 5, 5};
+        body = {200,300,20,20};
+        mouth = {body.x+10, body.y+7, 10, 6};
         moveD[0] = moveD[1] = moveD[2] = moveD[3] = 0;
-        for(int i = 0; i < 24; i++){
-            for(int j = 0; j < 24; j++){
-                std::cout<<_maps[24*i+j]<<" ";
-            }
-            std::cout<<std::endl;
-        }
     }
 
     void render(SDL_Renderer* renderer){
         SDL_SetRenderDrawColor(renderer, 255,255,0,255);
         SDL_RenderFillRect(renderer, &body);
         SDL_SetRenderDrawColor(renderer, 0,0,0,255);
-        SDL_RenderFillRect(renderer, &eye);
+        SDL_RenderFillRect(renderer, &mouth);
     }
 
     void control(SDL_Event* event){
         if(event->type == SDL_KEYDOWN){
             if(event->key.keysym.sym == SDLK_a){
-                moveD[0] = 1;
-                moveD[1] = 0;
-                moveD[2] = 0;
-                moveD[3] = 0;
+                if(!_maps[((body.y/20)*21)+(body.x/20)-1]){
+                    moveD[0] = 1;
+                    moveD[1] = moveD[2] = moveD[3] = 0;
+                }
             }
             if(event->key.keysym.sym == SDLK_d){
-                moveD[0] = 0;
-                moveD[1] = 1;
-                moveD[2] = 0;
-                moveD[3] = 0;
+                if(!_maps[((body.y/20)*21)+(body.x/20)+1]){
+                    moveD[1] = 1;
+                    moveD[0] = moveD[2] = moveD[3] = 0;
+                }
             }
             if(event->key.keysym.sym == SDLK_w){
-                moveD[0] = 0;
-                moveD[1] = 0;
-                moveD[2] = 1;
-                moveD[3] = 0;
+                if(!_maps[((body.y/20)*21)+(body.x/20)-21]){
+                    moveD[2] = 1;
+                    moveD[0] = moveD[1] = moveD[3] = 0;
+                }
             }
             if(event->key.keysym.sym == SDLK_s){
-                moveD[0] = 0;
-                moveD[1] = 0;
-                moveD[2] = 0;
-                moveD[3] = 1;
+                if(!_maps[((body.y/20)*21)+(body.x/20)+21]){
+                    moveD[3] = 1;
+                    moveD[0] = moveD[1] = moveD[2] = 0;
+                }
             }
         }
     }
 
     void update(){
         if(moveD[0]){
-            body.x--;
-            eye = {body.x+5, body.y+5, 5, 5};
+            bool istouch = false;
+            if(_maps[((body.y/20)*21)+(body.x/20)-1]){
+                istouch = true;
+            }
+            if(!istouch){
+                body.x-=20;
+                mouth = {body.x, body.y+7, 10, 6};
+            }
         }
         if(moveD[1]){
-            body.x++;
-            eye = {body.x+20, body.y+5, 5, 5};
+            bool istouch = false;
+            if(_maps[((body.y/20)*21)+(body.x/20)+1]){
+                istouch = true;
+            }
+            if(!istouch){
+                body.x+=20;
+                mouth = {body.x+10, body.y+7, 10, 6};
+            }
         }
         if(moveD[2]){
-            body.y--;
-            eye = {body.x+5, body.y+5, 5, 5};
+            bool istouch = false;
+            if(_maps[((body.y/20)*21)+(body.x/20)-21]){
+                istouch = true;
+            }
+            if(!istouch){
+                body.y-=20;
+                mouth = {body.x+7, body.y, 6, 10};
+            }
         }
         if(moveD[3]){
-            body.y++;
-            eye = {body.x+5, body.y+20, 5, 5};
+            bool istouch = false;
+            if(_maps[((body.y/20)*21)+(body.x/20)+21]){
+                istouch = true;
+            }
+            if(!istouch){
+                body.y+=20;
+                mouth = {body.x+7, body.y+10, 6, 10};
+            }
         }
     }
 
@@ -112,9 +126,9 @@ public:
 
 void mapRender(SDL_Renderer* renderer){
     SDL_Rect rect;
-    for(int i = 0; i < 24; i++){
-        for(int j = 0; j < 24; j++){
-            if(_maps[i*24+j]){
+    for(int i = 0; i < 21; i++){
+        for(int j = 0; j < 21; j++){
+            if(_maps[i*21+j]){
                 rect = {j*20, i*20, 20, 20};
                 SDL_SetRenderDrawColor(renderer, 0,0,255,255);
                 SDL_RenderFillRect(renderer, &rect);
@@ -125,12 +139,13 @@ void mapRender(SDL_Renderer* renderer){
 
 int main(int argc, char* argv[])
 {
-    SDL_Window* window = SDL_CreateWindow("apps", 100,100,480,480,false);
+    SDL_Window* window = SDL_CreateWindow("apps", 100,100,420,420,false);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
     player mplayer = player(&_maps);
 
     SDL_Event event;
+    int frame = SDL_GetTicks();
     while(true){
         SDL_SetRenderDrawColor(renderer, 0,0,0,255);
         SDL_RenderClear(renderer);
@@ -143,7 +158,11 @@ int main(int argc, char* argv[])
         }
         mplayer.control(&event);
 
-        mplayer.update();
+        if(SDL_GetTicks()-frame>=150){
+            mplayer.update();
+            std::cout<<mplayer.getBody()->x<<":"<<mplayer.getBody()->y<<std::endl;
+            frame = SDL_GetTicks();
+        }
 
 
         SDL_RenderPresent(renderer);
