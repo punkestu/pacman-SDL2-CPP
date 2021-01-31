8c995579c1991bbca80748512a1bbf0395ef6ec8
punkestu <pangestubima89@gmail.com>
2021-01-31 17:32:24 +0700

    menambahkan movement dan menambahkan map kosong

--- main.cpp
+++ main.cpp
@@ -7,14 +7,51 @@ struct vect2d{
     int x,y;
 };
 
+std::vector<bool> _maps = {
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+};
+
 class player{
 private:
     SDL_Rect body;
     SDL_Rect eye;
+    bool moveD[4];
+    std::vector<bool>* maps;
 public:
-    player(){
-        body = {0,0,30,30};
+    player(std::vector<bool>* maps){
+        body = {100,100,30,30};
         eye = {body.x+20, 5, 5, 5};
+        moveD[0] = moveD[1] = moveD[2] = moveD[3] = 0;
+        this->maps = maps;
+//        for(int i = 0; i < 24; i++){
+//            for(int j = 0; j < 24; j++){
+//                std::cout<<(*maps)[24*i+j]<<" ";
+//            }
+//            std::cout<<std::endl;
+//        }
     }
 
     void render(SDL_Renderer* renderer){
@@ -22,6 +59,54 @@ public:
         SDL_RenderFillRect(renderer, &body);
         SDL_SetRenderDrawColor(renderer, 0,0,0,255);
         SDL_RenderFillRect(renderer, &eye);
+    }
+
+    void control(SDL_Event* event){
+        if(event->type == SDL_KEYDOWN){
+            if(event->key.keysym.sym == SDLK_a){
+                moveD[0] = 1;
+                moveD[1] = 0;
+                moveD[2] = 0;
+                moveD[3] = 0;
+            }
+            if(event->key.keysym.sym == SDLK_d){
+                moveD[0] = 0;
+                moveD[1] = 1;
+                moveD[2] = 0;
+                moveD[3] = 0;
+            }
+            if(event->key.keysym.sym == SDLK_w){
+                moveD[0] = 0;
+                moveD[1] = 0;
+                moveD[2] = 1;
+                moveD[3] = 0;
+            }
+            if(event->key.keysym.sym == SDLK_s){
+                moveD[0] = 0;
+                moveD[1] = 0;
+                moveD[2] = 0;
+                moveD[3] = 1;
+            }
+        }
+    }
+
+    void update(){
+        if(moveD[0]){
+            body.x--;
+            eye = {body.x+5, body.y+5, 5, 5};
+        }
+        if(moveD[1]){
+            body.x++;
+            eye = {body.x+20, body.y+5, 5, 5};
+        }
+        if(moveD[2]){
+            body.y--;
+            eye = {body.x+5, body.y+5, 5, 5};
+        }
+        if(moveD[3]){
+            body.y++;
+            eye = {body.x+5, body.y+20, 5, 5};
+        }
     }
 
     SDL_Rect* getBody(){return &body;}
@@ -32,7 +117,7 @@ int main(int argc, char* argv[])
     SDL_Window* window = SDL_CreateWindow("apps", 100,100,480,480,false);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
-    player mplayer = player();
+    player mplayer = player(&_maps);
 
     SDL_Event event;
     while(true){
@@ -44,6 +129,9 @@ int main(int argc, char* argv[])
         if(event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)){
             break;
         }
+        mplayer.control(&event);
+
+        mplayer.update();
 
 
         SDL_RenderPresent(renderer);
